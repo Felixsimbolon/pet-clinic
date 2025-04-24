cb06f096a37c79dc1776e1181fe39a2e0d8209d6
Gordon P. Hutauruk <gordonp.hutauruk@Melurs-MacBook-Pro.local>
2025-04-25 04:02:35 +0700

    add dump.txt

--- pet_clinic/settings.py
+++ pet_clinic/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+# Add these at the top of your settings.py
+import os
+from dotenv import load_dotenv
+from urllib.parse import urlparse
+
+load_dotenv()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +33,7 @@ SECRET_KEY = 'django-insecure-xvx()buprl(3pk=3@clf&64kdf82%h-&m8aec&12fq0y0nl*$2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -72,10 +80,18 @@ WSGI_APPLICATION = 'pet_clinic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# Replace the DATABASES section of your settings.py with this
+DATABASE_URL='postgresql://neondb_owner:npg_wNGaE2ZkHv7M@ep-winter-wildflower-a4ox1ffc-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require'
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
     }
 }
 
