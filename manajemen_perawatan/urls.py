ad899fac95fcb307347139fee5ca63c77e7e6419
ErlandHafizh <erlandhafizh1@gmail.com>
2025-04-29 16:32:37 +0700

    nambahin role baru sama sesuaiin navbar dgn role

--- manajemen_perawatan/urls.py
+++ manajemen_perawatan/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('create/',               views.create_treatment_type, name='treatment_create'),
     path('update/<str:kode>/',    views.update_treatment_type, name='treatment_update'),
     path('delete/<str:kode>/',    views.delete_treatment_type, name='treatment_delete'),
+
+    path('perawat',                      views.list_treatment_types_perawat, name='treatment_list_perawat'),
+    path('create-perawat/',               views.create_treatment_type_perawat, name='treatment_create_perawat'),
+    path('update-perawat/<str:kode>/',    views.update_treatment_type_perawat, name='treatment_update_perawat'),
+    path('delete-perawat/<str:kode>/',    views.delete_treatment_type_perawat, name='treatment_delete_perawat'),
 ]
