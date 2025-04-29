38cbe04377c2572fc99b264af9bafa6112321f39
ErlandHafizh <erlandhafizh1@gmail.com>
2025-04-29 16:32:37 +0700

    nambahin role baru sama sesuaiin navbar dgn role

--- manajemen_obat/urls.py
+++ manajemen_obat/urls.py
@@ -1,4 +1,3 @@
-# manajemen_obat/urls.py
 from django.urls import path
 from . import views
 
@@ -8,4 +7,10 @@ urlpatterns = [
     path('update/<str:kode>/',          views.update_medicine,         name='medicine_update'),
     path('update-stock/<str:kode>/',    views.update_medicine_stock,   name='medicine_update_stock'),
     path('delete/<str:kode>/',          views.delete_medicine,         name='medicine_delete'),
+
+    path('perawat',                            views.list_medicines_perawat,          name='medicine_list_perawat'),
+    path('create-perawat/',                     views.create_medicine_perawat,         name='medicine_create_perawat'),
+    path('update-perawat/<str:kode>/',          views.update_medicine_perawat,         name='medicine_update_perawat'),
+    path('update-stock-perawat/<str:kode>/',    views.update_medicine_stock_perawat,   name='medicine_update_stock_perawat'),
+    path('delete-perawat/<str:kode>/',          views.delete_medicine_perawat,         name='medicine_delete_perawat'),
 ]
